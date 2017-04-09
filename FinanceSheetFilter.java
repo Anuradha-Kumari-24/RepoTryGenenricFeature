a7260aa0c64631b9cb17e9c32e5de458a69ac00d
Anuradha-Kumari-24 <itanuradha577@gmail.com>
2017-04-09 16:28:02 +0530

    Add files via upload

---
+++ FinanceSheetFilter.java
@@ -0,0 +1,48 @@
+package trialProject;
+
+public class FinanceSheetFilter {
+
+	private String countryNames;
+	
+	private String regionNames;
+	
+	private String cropNames;
+	
+	private String projectFullNames;
+
+	public String getCountryNames() {
+		return countryNames;
+	}
+
+	public void setCountryNames(String countryNames) {
+		this.countryNames = countryNames;
+	}
+
+	public String getRegionNames() {
+		return regionNames;
+	}
+
+	public void setRegionNames(String regionNames) {
+		this.regionNames = regionNames;
+	}
+
+	public String getCropNames() {
+		return cropNames;
+	}
+
+	public void setCropNames(String cropNames) {
+		this.cropNames = cropNames;
+	}
+
+	public String getProjectFullNames() {
+		return projectFullNames;
+	}
+
+	public void setProjectFullNames(String projectFullNames) {
+		this.projectFullNames = projectFullNames;
+	}
+	
+	
+	
+	
+}
---
+++ MainClass.java
@@ -0,0 +1,32 @@
+package trialProject;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainClass {
+	
+	
+	public static void main(String args[]) throws IllegalArgumentException, IntrospectionException, IllegalAccessException, InvocationTargetException, SecurityException, NoSuchMethodException
+	{
+		FinanceSheetFilter financeSheetFilter = new FinanceSheetFilter();
+		financeSheetFilter.setCountryNames("India");
+		
+		boolean condition = getConditionCreated(financeSheetFilter,"countryNames");
+		
+		System.out.println(condition);
+	}
+
+	private static boolean getConditionCreated(Object obj , String attributeName) throws IntrospectionException, IllegalArgumentException, IllegalAccessException,
+	InvocationTargetException, SecurityException {
+		PropertyDescriptor propertyDescriptor = new PropertyDescriptor(attributeName, obj.getClass());
+		Method getter = propertyDescriptor.getReadMethod();
+		
+		Object value = getter.invoke(obj);
+		
+		return (value != null && !value.toString().isEmpty());
+		
+	}
+
+}
